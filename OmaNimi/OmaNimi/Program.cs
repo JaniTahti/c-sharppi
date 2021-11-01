843de21b721ff5d5a46676fccccd589ff3b1b35a
JaniTheMilkMan <79130384+JaniTheMilkMan@users.noreply.github.com>
2021-11-01 11:21:55 +0200

    Add files via upload

---
+++ OmaNimi/OmaNimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OmaNimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minun nimi on Jani tähti");
+        }
+    }
+}
---
+++ suurinKolmesta/suurinKolmesta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace suurinKolmesta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lisää numero");
+            int luku1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lisää toka numero");
+            int luku2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lisää kolmas numero");
+            int luku3 = int.Parse(Console.ReadLine());
+        }
+    }
+}

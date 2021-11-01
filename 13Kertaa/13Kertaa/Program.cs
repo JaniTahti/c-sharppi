bac601bd3f170e6af038c25880de188343b9ce7e
JaniTheMilkMan <79130384+JaniTheMilkMan@users.noreply.github.com>
2021-11-01 11:24:47 +0200

    Add files via upload

---
+++ 13Kertaa/13Kertaa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _13Kertaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Anna luku joka on suurempi kuin 12");
+            x = int.Parse(Console.ReadLine());
+            if (x < 10)
+            {
+                Console.WriteLine("Error luku liian pieni");
+            }
+            else
+            
+        }
+    }
+}
---
+++ BonusPTS/BonusPTS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BonusPTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Anna numero niin saat bonus pisteet");
+            x = int.Parse(Console.ReadLine());
+            if (x == 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else if(x <=3)
+            {
+                Console.WriteLine("Lukusi on " + x * 10);
+            }
+            else if(x <=6) 
+            {
+                Console.WriteLine("Lukusi on " + x * 100);
+            }
+            else if(x <=8)
+            {
+                Console.WriteLine("Lukusi on " + x * 1000);
+            }
+            else if(x == 9)
+            {
+                Console.WriteLine("Error");
+            }
+           
+           
+
+        }
+    }
+}

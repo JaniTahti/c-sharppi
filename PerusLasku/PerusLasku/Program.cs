240c0139723fbe5c1debf40f7b466bc16c60cdb2
Jani tähti <79130384+JaniTheMilkMan@users.noreply.github.com>
2021-11-02 11:14:51 +0200

    Add files via upload

---
+++ PerusLasku/PerusLasku/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PerusLasku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna yksi numero niin numero lasketaan 4:jällä lasku tavalla");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Perus lasku toimitus");
+            Console.WriteLine(x + 13);
+            Console.WriteLine(x * 13);
+            Console.WriteLine(x / 13);
+            Console.WriteLine(x - 13);
+        }
+    }
+}

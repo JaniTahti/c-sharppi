d3e8854c1760076b426bfe214f92bb1b4e804e89
Jani tähti <79130384+JaniTheMilkMan@users.noreply.github.com>
2021-11-02 11:16:39 +0200

    Add files via upload

---
+++ 1+1/1+1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna numero");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Anna toinen numero");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Antamasi numero yhteen laskettuna on " +(x += y));
+        }
+    }
+}
---
+++ Fahrenheit/Fahrenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Anna celsius astikko");
+            int x = int.Parse(Console.ReadLine());
+
+           
+            Console.WriteLine(x * 1.8 + 32 + " Farenheit");
+        }
+    }
+}

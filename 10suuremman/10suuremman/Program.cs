41c6fecc73f3286f3984275bf69f9c2c29746373
Jani tähti <79130384+JaniTheMilkMan@users.noreply.github.com>
2021-11-03 11:09:31 +0200

    Add files via upload

---
+++ 10suuremman/10suuremman/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10suuremman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna luku joka on yhtäsuuri tai suurempi kuin 10");
+            int x = int.Parse(Console.ReadLine());
+            if(x < 10)
+            {
+                Console.WriteLine("Numero on liian pieni");
+            }
+            else
+            {
+                for (int i = 1; i < x; i++)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
---
+++ PosNeg/PosNeg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PosNeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Anna kokonais luku");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen kokonais luku");
+            x = int.Parse(Console.ReadLine());
+            if(x <= 0)
+            {
+                Console.WriteLine("Numerot ovat negatiivisia");
+            }
+            else if(x > 0)
+            {
+                Console.WriteLine("Luvut ovat positiivisa");
+            }
+            
+        }
+    }
+}

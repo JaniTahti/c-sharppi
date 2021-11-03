68b6984eda7d8be4445697a9a580dfa0a1351027
Jani tähti <79130384+JaniTheMilkMan@users.noreply.github.com>
2021-11-03 11:23:01 +0200

    Add files via upload

---
+++ PosNeg/PosNeg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PosNeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Anna kokonais luku");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna toinen kokonais luku");
+            int y = int.Parse(Console.ReadLine());
+            if(x <= 0)
+            {
+                Console.WriteLine("Numerot ovat negatiivisia");
+            }
+            else if(y > 0)
+            {
+                Console.WriteLine("Luvut ovat positiivisa");
+            }
+
+            { 
+            if (x > 0 && y < 0)
+                Console.WriteLine("Toinen on positiivinen ja toinen on negatiivinen");
+            if (x < 0 && y > 0)
+                Console.WriteLine("Toinen on positiivinen ja toinen on negatiivinen");
+            }
+        }
+    }
+}
